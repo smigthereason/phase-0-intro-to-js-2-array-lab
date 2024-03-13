e82e7962fb5ae9250d0c674e3946a9c60d5783cf
victor@victor <victor.dmaina@gmail.com>
2024-03-13 17:08:46 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function resetCats() {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+};
+function destructivelyAppendCat() {
+  cats.length = 0;
+  cats.push("Milo", "Otis", "Garfield", "Ralph")
+};
+function destructivelyPrependCat() {
+  cats.length = 0;
+  cats.push("Bob", "Milo", "Otis", "Garfield")
+};
+function destructivelyRemoveLastCat() {
+  cats.length = 0;
+  cats.push("Milo", "Otis")
+};
+function destructivelyRemoveFirstCat() {
+  cats.length = 0;
+  cats.push("Otis", "Garfield")
+};
+function appendCat(name = "Broom") {
+
+  return ["Milo", "Otis", "Garfield", "Broom"]
+};
+function prependCat(name = "Arnold") {
+  return ["Arnold", ...cats]
+};
+function removeLastCat() {
+  return cats.slice(0, -1);
+};
+function removeFirstCat() {
+  return cats.slice(1)
+};
